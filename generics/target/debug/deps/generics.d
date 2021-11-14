08b2ebb62304ec54c997df4d4fae26564d8a3955
alexandreLamarre <alex7285@gmail.com>
2021-11-14 11:17:59 -0500

    rectify adding sub-cargo package targets

--- generics/target/debug/deps/generics.d
+++
@@ -1,5 +0,0 @@
-/Users/alexandrelamarre/Desktop/repos/other/rustMandelbrot/generics/target/debug/deps/generics: src/main.rs
-
-/Users/alexandrelamarre/Desktop/repos/other/rustMandelbrot/generics/target/debug/deps/generics.d: src/main.rs
-
-src/main.rs:
--- generics/target/debug/generics.d
+++
@@ -1,1 +0,0 @@
-/Users/alexandrelamarre/Desktop/repos/other/rustMandelbrot/generics/target/debug/generics: /Users/alexandrelamarre/Desktop/repos/other/rustMandelbrot/generics/src/main.rs
